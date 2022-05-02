151380c0597cbbbe88948c5c960422cd9c59c2b8
Andrey Bazlov <buzzlove@mail.ru>
2022-05-03 00:20:53 +0500

    feat: start binary-search-tree

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,46 +1,71 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 class BinarySearchTree {
-
+  constructor(){
+    this.data = null;
+  }
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.data;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(value) {
+    this.data = addValue(this.data, value);
+    function addValue(node ,value){
+      if(!node){
+        return new Node(value);
+      }
+
+      if(node.value > value){
+        node.left = addValue(node.left, value);
+      } else {
+        node.right = addValue(node.rigth, value);
+      }
+      return node;
+    }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(value) {
+    return hasValue(this.data, value);
+
+    function hasValue(node, value){
+      switch(true){
+        case (!node): return false;
+        case (node.value === value): return true;
+        case (node.value > value): hasValue(node.left, value); break;
+        case (node.value < value): hasValue(node.right, value); break;
+      }   
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  find(value) {
+    return findValue(this.data, value);
+
+    function findValue(node, value){
+      switch(true){
+        case (!node): return null;
+        case (node.value === value): return value;
+        case (node.value > value): hasValue(node.left, value); return node;
+        case (node.value < value): hasValue(node.right, value); return node;
+      }   
+    }
   }
 
-  remove(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  remove(data) {
+    
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    
   }
 }
 
